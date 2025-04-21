94c54638dc3c708b417ad0ab8940b62c854b3515
Fernando Duarte <duarte@alum.mit.edu>
2025-04-20 21:35:27 -0400

    Refactor socket.js files to improve organization and maintainability

--- client/screen.js
+++ client/screen.js
@@ -1,5 +1,6 @@
 // screen.js
 // Load modules in the correct order
+document.write('<script src="/modules/shared/socket-utils.js"></script>');
 document.write('<script src="/modules/screen/dom.js"></script>');
 document.write('<script src="/modules/screen/game.js"></script>');
 document.write('<script src="/modules/screen/socket.js"></script>');
