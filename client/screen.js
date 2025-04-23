7a66a97b788c4aac355d12dad062ee975ac6e13d
Fernando Duarte <duarte@alum.mit.edu>
2025-04-23 08:12:18 -0400

    Complete CSS transition by removing css-loader.js

--- client/screen.js
+++ client/screen.js
@@ -1,6 +1,5 @@
 // screen.js
 // Load modules in the correct order
-document.write('<script src="/modules/shared/css-loader.js"></script>');
 document.write('<script src="/modules/shared/socket-utils.js"></script>');
 document.write('<script src="/modules/screen/dom.js"></script>');
 document.write('<script src="/modules/screen/game.js"></script>');
